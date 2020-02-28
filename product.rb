962afa6b7656b5d82b6f34e61aac8a3e50fe012b
Pat McKenna <pat@mckenna.rocks>
2020-02-28 15:52:36 +1100

    Move RRR scraping logic into its own file
    Until now we've only been scraping data from one site
    (renovaterestorerecycle.com.au), so all of the code has lived in a
    single file.
    
    Now I also wanna scrape data from a second site, so in preparation for
    that I'm tidying things up a bit. All the code specific to
    renovaterestorerecycle.com.au has now been moved into a new `RRR` module
    that lives in its own file. It's now invoked by calling the
    `RRR.get_products` method, which returns the products as an array.
    
    To add a new site, I'll do the same thing: give it its own module with
    a `.get_products` method, and merge the two returned arrays before doing
    all the availability checks and adding them to the database.

---
+++ product.rb
@@ -0,0 +1,1 @@
+Product = Struct.new(:name, :description, :price, :url)
--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 require "scraperwiki"
 require "mechanize"
+require_relative "scrape_rrr"
 require_relative "send_mail"
 
 raise "Notification email address is not set" if ENV["MORPH_NOTIFICATION_EMAIL_ADDRESS"].nil?
@@ -8,44 +9,6 @@ raise "Sendgrid API key is not set" if ENV["MORPH_SENDGRID_API_KEY"].nil?
 ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
 
 agent = Mechanize.new
-
-Product = Struct.new(:name, :description, :price, :url)
-
-def build_product(element:, agent:)
-  name_element = element.children.detect { |el| el["class"] == "entry-title" }
-  name = name_element.inner_text.strip
-
-  product_info_element = element.children.detect { |el| el["class"] == "product-info" }
-
-  description_elements = product_info_element.children.reject { |el| el["class"] == "price" }
-  description = description_elements.map(&:inner_text).map(&:strip).reject(&:empty?).join(" ")
-
-  price_element = product_info_element.children.detect { |el| el["class"] == "price" }
-  price = price_element.inner_text.downcase
-
-  link_element = element.children.detect { |el| el.name == "a" }
-  href = link_element["href"]
-  url = agent.agent.resolve(href).to_s
-
-  Product.new(name, description, price, url)
-end
-
-def get_products_from_page(page:, agent:)
-  puts "  Scraping page: #{page.uri}"
-
-  product_elements = page.search("article.type-rrr_stock")
-  puts "    #{product_elements.size} products found."
-
-  product_elements.map { |element| build_product(element: element, agent: agent) }
-end
-
-def get_next_page_url_from_page(page:, agent:)
-  next_page_link = page.at("a.page-numbers.next")
-
-  return nil if next_page_link.nil?
-
-  agent.agent.resolve(next_page_link["href"])
-end
 
 def load_existing_product_urls
   ScraperWiki.select("* from data")
@@ -71,24 +34,7 @@ def generate_mail_content(new_products:)
   [header, product_texts.join(separator), footer].join(separator)
 end
 
-products = []
-pages_scraped_count = 0
-
-start_page_url = "https://www.renovaterestorerecycle.com.au/category/sash-windows/?post_type=rrr_stock"
-next_page_url = start_page_url
-
-until next_page_url.nil? do
-  current_page = agent.get(next_page_url)
-
-  products_from_current_page = get_products_from_page(page: current_page, agent: agent)
-
-  products += products_from_current_page
-  pages_scraped_count += 1
-
-  next_page_url = get_next_page_url_from_page(page: current_page, agent: agent)
-end
-
-puts "\n#{pages_scraped_count} pages scraped. #{products.count} total products found."
+products = RRR.get_products(agent: agent)
 
 available_products = products.reject do |product|
   product.price.include?("sold")
